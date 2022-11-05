316103f53ffa50119243af5de42e38c1288d77a8
MfundoZA <jordantenza@gmail.com>
2022-11-05 10:56:05 +0200

    Added Asp.net project to Plannabelle

---
+++ Plannabelle-AspNetMVC/Plannabelle-AspNetMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PlannabelleClassLibrary.Data;
+
+namespace Plannabelle_AspNetMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
