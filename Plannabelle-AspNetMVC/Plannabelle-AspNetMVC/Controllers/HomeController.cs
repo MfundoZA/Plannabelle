afd71443c4b0e8a1426706f5acf861944a3ad71c
MfundoZA <jordantenza@gmail.com>
2022-11-07 19:00:00 +0200

    Exclueded Home page from authentication requirement

--- Plannabelle-AspNetMVC/Plannabelle-AspNetMVC/Controllers/HomeController.cs
+++ Plannabelle-AspNetMVC/Plannabelle-AspNetMVC/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Plannabelle_AspNetMVC.Models;
 using System.Diagnostics;
 
 namespace Plannabelle_AspNetMVC.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
