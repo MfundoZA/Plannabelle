b07cc2c5622e61a680f3edd1d698f7df719d996d
MfundoZA <jordantenza@gmail.com>
2022-11-09 17:58:51 +0200

    Added Semester data to Enrollment table

--- PlannabelleClassLibrary/PlannabelleClassLibrary/ViewModels/NewSemesterViewModel.cs
+++ PlannabelleClassLibrary/PlannabelleClassLibrary/ViewModels/NewSemesterViewModel.cs
@@ -11,10 +11,15 @@ namespace PlannabelleClassLibrary.ViewModels
     public class NewSemesterViewModel : BaseViewModel
     {
         public SemesterDataService SemesterDataService { get; set; } = new SemesterDataService(PlannabelleContextDbFactory.GetDbContext());
+        public EnrollmentDataService EnrollmentDataService { get; set; }// = new EnrollmentDataService(new PlannabelleDbContext());
 
         public bool WriteSemesterToDatabase(Semester newSemester)
         {
             SemesterDataService.Insert(newSemester);
+            SemesterDataService.dbContext.SaveChanges();
+
+            EnrollmentDataService.Insert(new Enrollment(User, null, newSemester, 0, 0, 0));
+            EnrollmentDataService.dbContext.SaveChanges();
 
             return true;
         }
