64c9363d3fcbf4be9a1c00535e48d4e93f070f59
MfundoZA <jordantenza@gmail.com>
2022-12-27 09:00:12 +0200

    Implented Authentication

--- Plannabelle/Controllers/AuthController.cs
+++ Plannabelle/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PlannabelleClassLibrary.Data;
+using PlannabelleClassLibrary.Models;
 using PlannabelleClassLibrary.ViewModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace Plannabelle.Controllers
 {
@@ -9,6 +12,12 @@ namespace Plannabelle.Controllers
     {
         public AuthViewModel AuthViewModel { get; set; } = new AuthViewModel();
         public PlannabelleDbContext DbContext { get; set; } = new PlannabelleDbContext();
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public AuthController(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
 
         // GET: AuthController/Register
         public ActionResult Register()
@@ -23,14 +32,20 @@ namespace Plannabelle.Controllers
         {
             try
             {
-                var username = collection["username"];
-                var email = collection["email"];
-                var password = collection["password"];
-                var confirmationPassword = collection["confimationpassword"];
+                var username = collection["username"].ToString();
+                var email = collection["email"].ToString();
+                var password = collection["password"].ToString();
+                var confirmationPassword = collection["confimationPassword"].ToString();
 
-               
+               if (String.Equals(password, confirmationPassword))
+               {
+                    var newStudent = new Student(username, email, password);
 
-                return RedirectToAction(nameof(Index));
+                    DbContext.Add<Student>(newStudent);
+                    DbContext.SaveChanges();
+               }
+
+                return RedirectToAction(nameof(Login));
             }
             catch
             {
@@ -49,14 +64,33 @@ namespace Plannabelle.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(IFormCollection collection)
         {
+            ViewBag.Message = "";
+
             try
             {
-                return RedirectToAction(nameof(Index));
+                var email = collection["email"].ToString();
+                var password = collection["password"].ToString();
+
+                if (DbContext.Students.Where<Student>(x => String.Equals(x.Email, email) && String.Equals(x.Password , password)).Any())
+                {
+                    var student = DbContext.Students.Where<Student>(x => String.Equals(x.Email, email) && String.Equals(x.Password, password)).First();
+
+                    HttpContextAccessor.HttpContext?.Session.SetInt32("userId", student.Id);
+                    HttpContextAccessor.HttpContext?.Session.SetString("email", student.Email);
+                }
+                else
+                {
+                    ViewBag.Message = "Incorrect email or password please try again";
+                }
+
+                
             }
             catch
             {
                 return View();
             }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
