105ffda1b2ea2757c8d37525f4a8c1b56b5deedc
MfundoZA <jordantenza@gmail.com>
2022-12-27 18:43:02 +0200

    Implemented password hashing function

--- Plannabelle/Controllers/AuthController.cs
+++ Plannabelle/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using PlannabelleClassLibrary.Data;
 using PlannabelleClassLibrary.Models;
 using PlannabelleClassLibrary.ViewModels;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace Plannabelle.Controllers
 {
@@ -40,6 +41,7 @@ namespace Plannabelle.Controllers
 
                if (String.Equals(password, confirmationPassword))
                {
+                    password = HashString(password);
                     var newStudent = new Student(username, email, password);
 
                     DbContext.Add<Student>(newStudent);
@@ -92,6 +94,28 @@ namespace Plannabelle.Controllers
             }
 
             return RedirectToAction("Index", "Home");
+        }
+        static string HashString(string text, string salt = "nf8n43nsd9s")
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+
+            // Uses SHA256 to create the hash
+            using (var sha = SHA256.Create())
+            {
+                // Convert the string to a byte array first, to be processed
+                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                // Convert back to a string, removing the '-' that BitConverter adds
+                string hash = BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", String.Empty);
+
+                return hash;
+            }
         }
     }
 }
