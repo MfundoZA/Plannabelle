b9c1e88f210c3b96ee916fa6583e233e76ef62b0
MfundoZA <jordantenza@gmail.com>
2023-01-01 09:05:22 +0200

    Created doubly linked list generic

---
+++ PlannabelleClassLibrary/Models/DoublyLinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannabelleClassLibrary.Models
+{
+    public class DoublyLinkedList<T>
+    {
+        public Node<T>? Head { get; set; }
+        public Node<T>? Tail { get; set; }
+        public int Length { get; set; }
+
+        public void addNode(T? data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (Head == null)
+            {
+                Head = Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
+            }
+
+            Length++;
+        }
+
+        public void deleteFirst()
+        {
+            if (Length == 0) throw new NullReferenceException("No elment in the list exists to delete!");
+
+            Head = Head.Next;
+            Head.Previous = null;
+            Length--;
+        }
+
+        public void deleteLast()
+        {
+            if (Length == 0) throw new NullReferenceException("No elment in the list exists to delete!");
+
+            Tail = Tail.Previous;
+            Tail.Next = null;
+            Length--;
+        }
+
+        public bool isEmpty()
+        {
+            return Length == 0;
+        }
+    }
+}
