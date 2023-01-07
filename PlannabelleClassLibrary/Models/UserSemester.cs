e894c2d93fb933c931cbea70dfb3b0f4920c4b5d
MfundoZA <jordantenza@gmail.com>
2023-01-07 07:13:16 +0200

    Created bridge tabel between User and Semester

---
+++ PlannabelleClassLibrary/Models/UserSemester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannabelleClassLibrary.Models
+{
+    public class UserSemester
+    {
+        public int UserId { get; set; }
+        public Student Student { get; set; } = null!;
+
+        public int SemesterId { get; set; }
+        public Semester Semester { get; set; } = null!;
+    }
+}
