d524e3359a401446c069c22ed49e45347e78f54c
MfundoZA <jordantenza@gmail.com>
2023-01-16 17:54:14 +0200

    Changed ClassHours to type double

--- PlannabelleClassLibrary/Models/Module.cs
+++ PlannabelleClassLibrary/Models/Module.cs
@@ -12,6 +12,6 @@ namespace PlannabelleClassLibrary.Models
         public string Code { get; set; } = null!;
         public string Name { get; set; } = null!;
         public int Credits { get; set; }
-        public int ClassHoursPerWeek { get; set; }
+        public double ClassHoursPerWeek { get; set; }
     }
 }
