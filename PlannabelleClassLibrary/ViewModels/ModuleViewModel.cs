606725ca2c1664bb612ead9d4587e5f496cb52d1
MfundoZA <jordantenza@gmail.com>
2023-01-16 17:54:55 +0200

    Add fields required to create a module

--- PlannabelleClassLibrary/ViewModels/ModuleViewModel.cs
+++ PlannabelleClassLibrary/ViewModels/ModuleViewModel.cs
@@ -8,6 +8,11 @@ namespace PlannabelleClassLibrary.ViewModels
 {
     public class ModuleViewModel
     {
-
+        public int Code { get; set; }
+        public string Name { get; set; } = null!;
+        public int Credits { get; set; }
+        public double ClassHoursPerWeek  { get; set; }
+        public double SelfStudyHoursPerWeek { get; set; }
+        public double SelfStudyHoursRemaining { get; set; }
     }
 }
